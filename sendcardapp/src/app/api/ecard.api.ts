dfdc9b1bc27fde42d16ed3f7039d01fab190d48a
Adrian Asagba <adrian.asagba@ctt.co.uk>
2018-07-13 16:21:43 +0100

    CA-1: add interface and request headers

--- sendcardapp/src/app/api/ecard.api.ts
+++ sendcardapp/src/app/api/ecard.api.ts
@@ -1,18 +1,34 @@
 import axios from 'axios';
 
-const BASE_URL ='';
+const BASE_URL ='https://arik-e-cards.p.mashape.com/';
 
 export interface ecardAPIRequest {
-
+  cardTemplateId: number;
+  from: string;
+  fromAddress: string;
+  message: string;
+  subject: string;
+  to: string;
+  toAddress: string;
 }
 
 export default {
 
   endpoints: {
     sendEcard: async (requestBody: ecardAPIRequest) => {
-      return axios.post(`${BASE_URL}/`, requestBody)
+
+      const configuration = {
+        headers: {
+          'X-Mashape-Key': 'Q3QLmntkE8mshcxKQXKcYJaDiPtJp1usRH3jsnGxZE6c6nkxAW',
+          'Accept': 'application/json',
+          'Content-Type': 'application/x-www-form-urlencoded',
+        }
+      };
+
+      return axios.post(`${BASE_URL}`, requestBody, configuration)
         .then(response => response);
     }
-  }
+  },
+
 };
 
